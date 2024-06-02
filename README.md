5a57d2a1b87991a206138631cd7c389e05a19661
Can Unsal <unsalcan35@gmail.com>
2024-06-02 18:09:49 +0300

    feat: port to kwin/plasma 6

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A kwin script to send active window to a numbered virtual desktop and refocus on
 The default behaviour KDE's window to desktop functionality keeps the focus on the moved window, which is not in current desktop anymore.
 
 ## Install
-Clone the repository and install it via `kpackagetool5`.
+Clone the repository and install it via `kpackagetool6`.
 ```shell
-kpackagetool5 --type=KWin/Script -i <dir>
+kpackagetool6 --type=KWin/Script -i <dir>
 ```
--- contents/code/main.js
+++ contents/code/main.js
@@ -1,31 +1,29 @@
-function getTopmostWindow() {
-  let clients = workspace.clientList();
-  let desktopCurrent = workspace.currentDesktop;
-
-  // Iterate in reverse order, last item means topmost.
-  for (let i=clients.length-1; i > -1; --i) {
+function getTopmostWindowFromDesktop(desktop) {
+  // Last item is the topmost window.
+  let clients = workspace.stackingOrder;
+  for (let i = clients.length - 1; i > -1; --i) {
     let c = clients[i];
-    if (c.desktop == desktopCurrent && !c.minimized) {
+    if (c.windowType === 0 && !c.minimized && c.desktops[0] === desktop && c.caption !== "Wayland to X Recording bridge — Xwayland Video Bridge") {
       return c;
     }
   }
-
   return null;
 }
 
-function moveWindowToDesktop(window, target) {
-  if (window) {
-    window.desktop = target;
+function moveWindowToDesktop(window, desktop) {
+  if (window && desktop) {
+    window.desktops = desktop;
   }
 }
 
 function moveWindowToDesktopAndRefocus(target) {
-  moveWindowToDesktop(workspace.activeClient, target);
+  desktop = workspace.desktops[target];
+  moveWindowToDesktop(workspace.activeWindow, desktop);
   // If there are no windows in current desktop, reset focus.
-  workspace.activeClient = getTopmostWindow();
+  workspace.activeWindow = getTopmostWindowFromDesktop(workspace.currentDesktop);
 }
 
-registerShortcut('moveWindowToDesktopAndRefocus-1', 'Window to Desktop 1 And Refocus', 'Meta+!', function(){moveWindowToDesktopAndRefocus(1);});
-registerShortcut('moveWindowToDesktopAndRefocus-2', 'Window to Desktop 2 And Refocus', 'Meta+@', function(){moveWindowToDesktopAndRefocus(2);});
-registerShortcut('moveWindowToDesktopAndRefocus-3', 'Window to Desktop 3 And Refocus', 'Meta+#', function(){moveWindowToDesktopAndRefocus(3);});
-registerShortcut('moveWindowToDesktopAndRefocus-4', 'Window to Desktop 4 And Refocus', 'Meta+$', function(){moveWindowToDesktopAndRefocus(4);});
+registerShortcut('moveWindowToDesktopAndRefocus-1', 'Window to Desktop 1 And Refocus', 'Meta+!', function () { moveWindowToDesktopAndRefocus(0); });
+registerShortcut('moveWindowToDesktopAndRefocus-2', 'Window to Desktop 2 And Refocus', 'Meta+@', function () { moveWindowToDesktopAndRefocus(1); });
+registerShortcut('moveWindowToDesktopAndRefocus-3', 'Window to Desktop 3 And Refocus', 'Meta+#', function () { moveWindowToDesktopAndRefocus(2); });
+registerShortcut('moveWindowToDesktopAndRefocus-4', 'Window to Desktop 4 And Refocus', 'Meta+$', function () { moveWindowToDesktopAndRefocus(3); });
